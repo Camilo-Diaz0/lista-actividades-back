068d9242b05edc5708742088f1de3c23b97c91bc
Camilo Diaz <diazc.pr7966@gmail.com>
2023-03-01 22:27:50 -0500

    agregando la dependecia de spring security

---
+++ src/main/java/com/example/ListadeTareas/security/WebSecurity.java
@@ -0,0 +1,10 @@
+package com.example.ListadeTareas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class WebSecurity{
+//    @Bean
+//    SecurityFilterChain securityFilterChain(H)
+}
