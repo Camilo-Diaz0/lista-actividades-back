02e231d70b9fe2e13f6b27a904aa499335e3a8d6
Camilo Diaz <diazc.pr7966@gmail.com>
2023-03-18 19:55:12 -0500

    agregando jwt

--- src/main/java/com/example/ListadeTareas/controller/UsuariosController.java
+++ src/main/java/com/example/ListadeTareas/controller/UsuariosController.java
@@ -1,26 +1,33 @@
 package com.example.ListadeTareas.controller;
 
+import com.example.ListadeTareas.dto.AuthRequest;
 import com.example.ListadeTareas.entities.Actividades;
 import com.example.ListadeTareas.entities.Usuarios;
 import com.example.ListadeTareas.repository.UsuariosRepository;
+import com.example.ListadeTareas.services.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuariosController {
     @Autowired
+    private JwtService jwtService;
+    @Autowired
     private UsuariosRepository repository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("/crear")
     public ResponseEntity<Usuarios> registrar(@RequestBody Usuarios usuarios){
@@ -34,4 +41,14 @@ public class UsuariosController {
         repository.save(usuarios);
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/autenticar")
+    public String autenticarObtenerToken(@RequestBody AuthRequest authRequest){
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if(authenticate.isAuthenticated()){
+            return jwtService.generarToken(authRequest.getUsername());
+        }
+        return "no registrado";
+    }
 }
+
--- src/main/java/com/example/ListadeTareas/security/WebSecurity.java
+++ src/main/java/com/example/ListadeTareas/security/WebSecurity.java
@@ -1,18 +1,24 @@
 package com.example.ListadeTareas.security;
 
+import com.example.ListadeTareas.filter.JwtAuthFilter;
 import com.example.ListadeTareas.services.JpaUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -20,19 +26,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class WebSecurity{
 
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf().disable()
-                .authorizeHttpRequests().requestMatchers("/usuarios/crear","/api/**").permitAll()
+                .authorizeHttpRequests().requestMatchers("/usuarios/**").permitAll()
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET,"/api/actividades")
-//                .anyRequest()
+//                .requestMatchers("/api/actividades")
+                .anyRequest()
                 .authenticated()
-                .and().formLogin()
-                .and().build();
-
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).build();
 
     }
     @Bean
@@ -51,5 +63,9 @@ public class WebSecurity{
         authenticationProvide.setUserDetailsService(userDetailsService());
         authenticationProvide.setPasswordEncoder(passwordEncoder());
         return authenticationProvide;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
