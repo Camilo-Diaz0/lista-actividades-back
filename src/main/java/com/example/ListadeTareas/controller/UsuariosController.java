9c6732d0ebf5a6034e132779110966e22d7a7829
Camilo Diaz <diazc.pr7966@gmail.com>
2023-03-24 17:27:36 -0500

    eliminando imports que no se usaban

--- src/main/java/com/example/ListadeTareas/controller/UsuariosController.java
+++ src/main/java/com/example/ListadeTareas/controller/UsuariosController.java
@@ -2,21 +2,13 @@ package com.example.ListadeTareas.controller;
 
 import com.example.ListadeTareas.dto.AuthRequest;
 import com.example.ListadeTareas.entities.Usuarios;
-import com.example.ListadeTareas.repository.UsuariosRepository;
-import com.example.ListadeTareas.services.ActividadesServices;
-import com.example.ListadeTareas.services.JwtService;
 import com.example.ListadeTareas.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/usuarios")
--- src/main/java/com/example/ListadeTareas/services/ActividadesServices.java
+++ src/main/java/com/example/ListadeTareas/services/ActividadesServices.java
@@ -1,21 +1,13 @@
 package com.example.ListadeTareas.services;
 
-import com.example.ListadeTareas.dto.AuthRequest;
 import com.example.ListadeTareas.entities.Actividades;
 import com.example.ListadeTareas.entities.Usuarios;
 import com.example.ListadeTareas.repository.ActividadRepository;
 import com.example.ListadeTareas.repository.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
